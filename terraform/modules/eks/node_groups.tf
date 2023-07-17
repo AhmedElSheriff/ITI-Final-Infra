3242cfe8c993dd32cad57ab2bf6a5df9b155c7a0
Ahmed Abdel Shafy <a.mohamedelsherif@gmail.com>
2023-07-17 18:44:17 +0300

    edits

--- terraform/modules/eks/node_groups.tf
+++ terraform/modules/eks/node_groups.tf
@@ -6,7 +6,7 @@ resource "aws_eks_node_group" "private_cluster_ng" {
   version = "1.23"
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 1
     min_size     = 1
   }
 
